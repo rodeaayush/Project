980cfc0cf21bda3790553c6dbb1f85eb647db4ba
rodeaayush <rodeaayush2@gmail.com>
2024-03-05 18:59:16 +0530

    daksdjfad

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ansible-master" {
   instance_type =  var.instance_type
   ami           = "ami-07d9b9ddc6cd8dd30"  # Replace with your AMI ID
   tags = {
-        name = "ansible-master"  
+        Name = "ansible-master"  
         Envoirnment = "Ansible Server"
         Project = "MotoGP"
         Owner = "Aayush"
@@ -18,7 +18,7 @@ resource "aws_instance" "node_servers" {
   instance_type =  var.instance_type
   ami           = "ami-07d9b9ddc6cd8dd30"  # Replace with your AMI ID
   tags = {
-        name = "node_server-${count.index + 1}"  
+        Name = "node_server-${count.index + 1}"  
         Envoirnment = "Application Server"
         Project = "MotoGP"
         Owner = "Aayush"
@@ -30,7 +30,7 @@ resource "aws_instance" "monitoring_server" {
   instance_type =  var.instance_type
   ami           = "ami-07d9b9ddc6cd8dd30"  # Replace with your AMI ID
   tags = {
-        name = "monitoring_server"
+        Name = "monitoring_server"
         Envoirnment = "Grafana/Prometheus Server"
         Project = "MotoGP"
         Owner = "Aayush"
